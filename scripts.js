22531b923540a667163618c74a2d1a66e4bdd723
ccarni <skylanderchance48@gmail.com>
2023-01-12 13:17:09 -0700

    Update scripts.js

--- scripts.js
+++ scripts.js
@@ -1,7 +1,13 @@
 let grid = document.getElementById("grid");
 let scoreDisplay = document.getElementById("score");
+let gridArray = new Array(grid.childNodes);
 let gridWidth = 20, gridHeight = 20;
-let snakeSpeed = 75;
+
+let snakeSpeed;
+let direction;
+let score;
+let updateId;
+let snakes, apples = [];
 
 let restartButton = document.createElement("button");
 restartButton.classList.add("restart");
@@ -9,21 +15,16 @@ restartButton.innerHTML = "Restart";
 restartButton.addEventListener("click", initialize);
 document.querySelector("body").appendChild(restartButton);
 
-let direction;
-let score;
-let updateId;
-let snakes, apples = [];
 
 class Snake {
-    constructor(headPosition, direction, length) {
-        const gridArray = new Array(grid.childNodes);
+    constructor(headPosition, direction, length, _gridArray) {
         this.pos = headPosition;
         this.dir = direction;
         this.length = length;
         this.dead = false;
-
         this.body = [];
 
+        // Create the body
         for (var i = 0; i < length; i++){
             if (this.dir == "RIGHT") {
                 this.body.push(headPosition - i);
@@ -36,25 +37,16 @@ class Snake {
             }
         }
 
-        for (let i = 0; i < gridArray[0].length; i++) {
+        for (let i = 0; i < _gridArray[0].length; i++) {
             if (this.body.includes(i)) { 
-                gridArray[0][i].classList.add("snake");
+                _gridArray[0][i].classList.add("snake");
             }
         }
 
     }
 
-    update(direction) {
+    update(direction, _gridArray) {
         this.dir = direction;
-        const gridArray = new Array(grid.childNodes);
-
-        // Delete the snake from its current position
-        for (let i = 0; i < gridArray[0].length; i++) {
-            if (this.body.includes(i)) { 
-                gridArray[0][i].classList.remove("snake");
-            }
-        }
-
         // move the rest of the body
         for (let i = this.body.length - 1; i > 0; i--) {
             this.body[i] = this.body[i - 1];
@@ -68,23 +60,14 @@ class Snake {
 
         this.pos = this.body[0];
 
-        // Add the body at the new position 
-        for (let i = 0; i < gridArray[0].length; i++) {
-            if (this.body.includes(i)) { 
-                gridArray[0][i].classList.add("snake");
-            }
-        }
-        // Check if the snake has collided with its own body
         if (this.body.slice(1).includes(this.body[0])){
             this.dead = true;
         }
 
-        // Check vertical bounds
         if (this.body[0] < 0 || this.body[0] >= gridWidth * gridHeight){
            this.dead = true; 
         }
 
-        // Check left bounds
         if ((this.body[0] % 20 == 0) && (this.dir == "LEFT")) {
             this.dead = true;
         }
@@ -95,60 +78,67 @@ class Snake {
         
     }
 
+    draw (_gridArray) {
+        for (let i = 0; i < _gridArray[0].length; i++) {
+            if (_gridArray[0][i].classList.contains("snake")) { 
+                _gridArray[0][i].classList.remove("snake");
+            }
+        }
+
+        for (let i = 0; i < _gridArray[0].length; i++) {
+            if (this.body.includes(i)) { 
+                _gridArray[0][i].classList.add("snake");
+            }
+        }
+    }
+
     elongate() {
         if (this.dir == "RIGHT") this.body.push(this.body[this.body.length - 1] - 1);
         else if (this.dir == "LEFT") this.body.push(this.body[this.body.length - 1] + 1);
         else if (this.dir == "UP") this.body.push(this.body[this.body.length - 1] + 20);
         else if (this.dir == "DOWN") this.body.push(this.body[this.body.length - 1] - 20);
+
+        this.length++;
     }
 }
 
 class Apple {
-    constructor(position) {
+    constructor(position, _gridArray) {
         this.pos = position;
-        const gridArray = new Array(grid.childNodes);
-        for (let i = 0; i < gridArray[0].length; i++) {
+        for (let i = 0; i < _gridArray[0].length; i++) {
             if (this.pos == i) { 
-                gridArray[0][i].classList.add("apple");
+                _gridArray[0][i].classList.add("apple");
             }
         }
     }
 
-    move() {
-        const gridArray = new Array(grid.childNodes);
-        for (let i = 0; i < gridArray[0].length; i++) {
+    move(_gridArray) {
+        for (let i = 0; i < _gridArray[0].length; i++) {
             if (this.pos == i) { 
-                gridArray[0][i].classList.remove("apple");
+                _gridArray[0][i].classList.remove("apple");
             }   
         }
 
         this.pos = Math.floor(Math.random() * gridWidth * gridHeight);
 
-        for (let i = 0; i < gridArray[0].length; i++) {
+        for (let i = 0; i < _gridArray[0].length; i++) {
             if (this.pos == i) { 
-                gridArray[0][i].classList.add("apple");
+                _gridArray[0][i].classList.add("apple");
             }
         }
     }
 }
 
 function initialize() {
-
-    grid = document.getElementById("grid");
-    scoreDisplay = document.getElementById("score");
     scoreDisplay.innerHTML = "Score: 0";
     snakeSpeed = 75;
-    gridWidth = 20, gridHeight = 20;
-
     direction = "RIGHT";
     score = 0;
-
-    // Empty the grid
+    
     while (grid.firstChild) {
         grid.removeChild(grid.firstChild);
     }
-
-    // Setup the grid
+    
     for (let i = 0; i < gridWidth * gridHeight; i++) {
         const unit = document.createElement("div");
         unit.classList.add("unit");
@@ -157,8 +147,8 @@ function initialize() {
 
     snakes = [];
     apples = [];
-    snakes.push(new Snake(5, "RIGHT", 4));
-    apples.push(new Apple(Math.floor(Math.random() * gridWidth * gridHeight)))
+    snakes.push(new Snake(5, "RIGHT", 4, gridArray));
+    apples.push(new Apple(Math.floor(Math.random() * gridWidth * gridHeight), gridArray))
     
     document.addEventListener('keydown', updateDirection);
     clearTimeout(updateId);
@@ -169,35 +159,35 @@ function update() {
     updateId = setTimeout(update, snakeSpeed);
     snakeSpeed = 75 - score*2;
     snakes.forEach(snake => {
-        snake.update(direction); 
-
+        snake.update(direction, gridArray); 
 
         if (snake.dead) {
             clearTimeout(updateId);
             scoreDisplay.innerHTML = "You Lost, Your Score is " + score;
-            
+            return score;
         }
+
+        snake.draw(gridArray);
 
         apples.forEach(apple => {
             if (apple.pos == snake.pos){
                 snake.elongate();
-                apple.move();
+                apple.move(gridArray);
                 score++;
                 scoreDisplay.innerHTML = "Score: " + score;
             } 
         });
     });
-
 }
 
 function updateDirection(e) {
-    if (e.key == "ArrowLeft") {
+    if (e.key == "ArrowLeft" || e.key == "a") {
         direction = "LEFT";
-    } else if (e.key == "ArrowRight") {
+    } else if (e.key == "ArrowRight" || e.key == "d") {
         direction = "RIGHT";
-    } else if (e.key == "ArrowUp") {
+    } else if (e.key == "ArrowUp" || e.key == "w") {
         direction = "UP";
-    } else if (e.key == "ArrowDown") {
+    } else if (e.key == "ArrowDown" || e.key == "s") {
         direction = "DOWN";
     }
 }
